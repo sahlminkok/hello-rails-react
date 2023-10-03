4bfd4b4bd8a1980e1001defc35023c8945bf5d05
sahlminkok <sahlminkok7@gmail.com>
2023-10-03 17:39:51 +0100

    Add API route for random greetings in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'root#index'
+
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/random', to: 'greetings#random_greeting'
+    end
+  end
 end
