41f90cb11a4d71276ecdb17a404f5f107d649f64
sahlminkok <sahlminkok7@gmail.com>
2023-10-03 17:48:18 +0100

    Fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::GreetingsController < ApplicationController
   def random_greeting
-    random_message = Message.order("RANDOM()").first
+    random_message = Greeting.order('RANDOM()').first
     if random_message
-      render json: { greeting: random_message.content }
+      render json: { greeting: random_message.message }
     else
-      render json: { error: "No greetings available." }, status: :not_found
+      render json: { error: 'No greetings available.' }, status: :not_found
     end
   end
 end
